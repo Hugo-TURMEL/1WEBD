e634cce9aaca61f9ff915d7b06c0ea5a6b86d5a6
Hugo Turmel <hugo.turmel@supinfo.com>
2025-11-24 16:30:17 +0100

    jour 1 done

---
+++ jour1.js
@@ -0,0 +1,40 @@
+// exo 1
+//prix = prompt("Saisissez un prix")
+//taux = prompt("Saisissez un taux de TVA")   
+//nb = prompt("Combien d'articles voulez-vous ?")
+
+//res = (nb*prix)*taux
+//console.log(res)
+
+
+//exo 2.1
+// a = prompt("entrez une valeur")
+// b = prompt("entrez une autre valeur")
+// res=a , a=b , b=res 
+// console.log(a,b)
+
+//exo 2.2
+// a = prompt("entrez une valeur")
+// b = prompt("entrez une autre valeur")
+// res = a
+// a=b 
+// b=res
+// console.log(a, b)
+
+//exo 2.3
+// a = Number(prompt("entrez une valeur"))
+// b = Number(prompt("entrez une autre valeur"))
+// res = a + (b-a)
+// ans = b - (b-a)
+// console.log(res, ans)
+
+
+//exo 3
+// x = prompt("entrez une valeur")
+// y = prompt("entrez une valeur")
+// z = prompt("entrez une valeur")
+// res=x
+// x=y
+// y=z
+// z=res
+// console.log(x, y, z)
